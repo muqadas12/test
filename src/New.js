ddedacb09170a04891ad48c375d396a10fc9f133
muqadas12 <60438885+muqadas12@users.noreply.github.com>
2021-08-29 12:09:45 +0500

    'ne'

---
+++ src/New.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>helo world</h1>
+     <form>
+       <label>First name</label>
+       <input type="text" placeholder= "name"/>
+       <label>last name</label>
+       <input type="text" placeholder= "name"/>
+       <label>Email:</label>
+       <input type="text" placeholder= "email"/>
+
+     </form>
+    </div>
+  );
+}
+
+export default App;
